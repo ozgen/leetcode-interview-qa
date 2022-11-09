23431c41995ea3acdb415639cb923d13cdee2a50
ozgen <mehmet.ozgen@deepnetwork.com>
2022-11-09 15:09:14 +0300

    day2- data structure

---
+++ datastructures/src/main/java/arrays/questions/RemoveElement.java
@@ -0,0 +1,28 @@
+package arrays.questions;
+
+public class RemoveElement {
+
+    public int removeElement(int[] nums, int val) {
+        int pivot = 0;
+        int[] arr= new int[nums.length];
+        if (nums.length == 0) return pivot;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != val) {
+                arr[pivot++] = nums[i];
+            }
+        }
+        for(int i=0;i<nums.length;i++){
+            nums[i]=arr[i];
+        }
+        return pivot;
+    }
+
+    public static void main(String[] args) {
+        RemoveElement removeElement = new RemoveElement();
+        int [] nums = new int[] {1,1,1,2,2,2,3,3,3};
+        int val = 2;
+        int retVal = removeElement.removeElement(nums,val);
+        System.out.println(retVal);
+
+    }
+}
