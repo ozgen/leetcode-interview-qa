e4e1047d3bb0b5c1e89f9c3aba4cdf50e968f417
ozgen <mehmet.ozgen@deepnetwork.com>
2022-11-15 18:35:21 +0300

    day5- data structure

--- datastructures/src/main/java/linklists/questions/MergeTwoSortedList.java
+++ datastructures/src/main/java/linklists/questions/MergeTwoSortedList.java
@@ -25,4 +25,19 @@ public class MergeTwoSortedList {
         cur.next = l1 == null ? l2 : l1;
         return start.next;
     }
+
+    public static void main(String[] args) {
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(5);
+        ListNode head2 = new ListNode(1);
+        head2.next = new ListNode(3);
+        head2.next.next = new ListNode(4);
+        MergeTwoSortedList mergeTwoSortedList = new MergeTwoSortedList();
+        ListNode mergedList = mergeTwoSortedList.mergeTwoLists(head, head2);
+        while (mergedList!=null){
+            System.out.println(mergedList.val);
+            mergedList = mergedList.next;
+        }
+    }
 }
